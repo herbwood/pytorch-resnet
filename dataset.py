cb0884d72264778d1f383c35c3e793c8bcf8870d
herbwood <herbwood27@gmail.com>
2021-06-29 21:59:59 +0900

    changed storing labels

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from PIL import Image
 
 import torch
 from torch.utils import data
@@ -16,7 +17,7 @@ class CIFAR10Dataset(Dataset):
         self.train_list = ['data_batch_1', 'data_batch_2', 'data_batch_3',
                       'data_batch_4', 'data_batch_5',]
         self.val_list = ["test_batch"]
-
+        
         self.basepath = basepath
         self.transform = transform
         self.data = []
@@ -34,15 +35,17 @@ class CIFAR10Dataset(Dataset):
             filepath = os.path.join(self.basepath, filename)
             data_dict = unpickle(filepath)
             self.data.append(data_dict[b'data'])
-            self.labels.append(data_dict[b'labels'])
+            self.labels.extend(data_dict[b'labels'])
 
-        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32).transpose((0, 2, 3, 1))
+        self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
+        self.data = self.data.transpose((0, 2, 3, 1))
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         image, label = self.data[idx], self.labels[idx]
+        image = Image.fromarray(image)
 
         if self.transform is not None:
             image = self.transform(image)
