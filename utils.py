2c15cade65ade194a119175b737731efd3682f39
herbwood <herbwood27@gmail.com>
2021-07-02 21:37:40 +0900

    added cutmix

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import pickle
 import requests
 import tarfile
 import logging 
+import numpy as np 
 
 import math
 from torch.nn.functional import one_hot
@@ -150,6 +151,41 @@ def label_smoothing_loss(pred, gold, device, smoothing_eps=0.1):
     loss = -(one_hot * log_prb).sum(dim=1)
 
     return loss.mean()
+
+def rand_bbox(size, lam):
+    W = size[2]
+    H = size[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # uniform
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+
+def cutmix(input, target, model, criterion, args):
+    r = np.random.rand(1)
+    if args.beta > 0 and r < args.cumtix_prob:
+        lam = np.random.beta(args.beta, args.beta)
+        rand_index = torch.randperm(input.size()[0]).cuda()
+        target_a = target
+        target_b = target[rand_index]
+        bbx1, bby1, bbx2, bby2 = rand_bbox(input.size(), lam)
+        input[:, :, bbx1:bbx2, bby1:bby2] = input[rand_index, :, bbx1:bbx2, bby1:bby2]
+
+        lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (input.size()[-1] * input.size()[-2]))
+        output = model(input)
+        loss = criterion(output, target_a) * lam + criterion(output, target_b) * (1. - lam)
+
+        return loss
+    return 
 
 def optimizer_select(model, args):
 
